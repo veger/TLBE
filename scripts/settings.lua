c64ea4c36fea627618b82271bfe79e7e58ab2399
Maarten Bezemer <maarten.bezemer@gmail.com>
2020-05-26 14:08:46 +0200

    Initialize playerSettings when TBLE is enabled later in the game

--- scripts/settings.lua
+++ scripts/settings.lua
@@ -2,32 +2,37 @@ if not tlbe then tlbe = {} end
 
 function tlbe.reload_settings(event)
     local player = game.players[event.player_index]
-    local playerSettings = settings.get_player_settings(player)
+    local guiSettings = settings.get_player_settings(player)
 
-    local settings = global.playerSettings[event.player_index] or {}
-    settings.enabled = playerSettings["tlbe-enabled"].value;
+    local playerSettings = global.playerSettings[event.player_index] or {}
+    playerSettings.enabled = guiSettings["tlbe-enabled"].value;
 
-    if settings.enabled then
-        settings.noticesEnabled = playerSettings["tlbe-notices-enabled"].value;
-        settings.saveFolder = playerSettings["tlbe-save-folder"].value
-        settings.followPlayer = playerSettings["tlbe-follow-player"].value
-        settings.screenshotInterval = (60 *
-                                          playerSettings["tlbe-speed-increase"]
-                                              .value) /
-                                          playerSettings["tlbe-frame-rate"]
-                                              .value
-        settings.width = playerSettings["tlbe-resolution-x"].value;
-        settings.height = playerSettings["tlbe-resolution-y"].value;
+    if playerSettings.enabled then
+        playerSettings.noticesEnabled = guiSettings["tlbe-notices-enabled"]
+                                            .value;
+        playerSettings.saveFolder = guiSettings["tlbe-save-folder"].value
+        playerSettings.followPlayer = guiSettings["tlbe-follow-player"].value
+        playerSettings.screenshotInterval =
+            (60 * guiSettings["tlbe-speed-increase"].value) /
+                guiSettings["tlbe-frame-rate"].value
+        playerSettings.width = guiSettings["tlbe-resolution-x"].value;
+        playerSettings.height = guiSettings["tlbe-resolution-y"].value;
 
-        if settings.screenshotInterval < 1 then
-            settings.enabled = false
-            playerSettings["tlbe-enabled"] = {value = false}
+        if playerSettings.screenshotInterval < 1 then
+            playerSettings.enabled = false
+            guiSettings["tlbe-enabled"] = {value = false}
 
             tlbe.log({"tlbe-disabled"});
             tlbe.log({"err_interval"});
         end
     end
 
-    global.playerSettings[event.player_index] = settings
+    global.playerSettings[event.player_index] = playerSettings
+
+    if playerSettings.enabled and playerSettings.centerPos == nil then
+        -- initialize player settings if not yet done to prevent issues later
+        tlbe.follow_player(playerSettings, player)
+    end
+
     tlbe.log({"err_generic", "reload_settings", "Settings loaded"});
 end
