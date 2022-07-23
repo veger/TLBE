aa1dc070a5c7de53f665b0aba1328abd0a5879de
Matthew Pocock <turingatemyhamster@gmail.com>
2022-07-23 15:23:12 +0100

    Added a daytime toggle to cameras.

---
+++ migrations/tlbe-1.4.4.lua
@@ -0,0 +1,19 @@
+if global.playerSettings == nil then
+    goto SkipMigration
+end
+
+-- Set new camera setting to its previous (default) value
+for player_index, _ in pairs(game.players) do
+    local playerSettings = global.playerSettings[player_index]
+    if playerSettings == nil then
+        goto NextPlayer
+    end
+
+    for _, camera in pairs(playerSettings.cameras) do
+        camera.alwaysDay = true
+    end
+
+    ::NextPlayer::
+end
+
+::SkipMigration::
